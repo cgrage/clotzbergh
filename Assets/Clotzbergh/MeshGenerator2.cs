e541429a494a1b3562079a0fb81bffe613e781fa
Christian Grage <christian.grage@gmx.de>
2024-09-29 00:27:41 +0200

    Some real mesh generation

--- Assets/Clotzbergh/MeshGenerator2.cs
+++ Assets/Clotzbergh/MeshGenerator2.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,28 +8,64 @@ public class MeshGenerator2
         // 
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public MeshBuilder GenerateTerrainMesh(WorldChunk worldChunk, int lod)
     {
-        return GenerateCuboid(WorldChunk.Size);
+        CellWallBuilder builder = new(WorldChunk.Size, WorldChunk.KlotzCount);
+
+        for (int z = 0; z < WorldChunk.KlotzCountZ; z++)
+        {
+            for (int y = 0; y < WorldChunk.KlotzCountY; y++)
+            {
+                for (int x = -1; x < WorldChunk.KlotzCountX; x++)
+                {
+                    bool b1t = worldChunk.Get(x + 0, y, z).IsSeeThrough;
+                    bool b2t = worldChunk.Get(x + 1, y, z).IsSeeThrough;
+                    if (b1t != b2t) { builder.AddWallX(x, x + 1, y, z, b1t); }
+                }
+            }
+        }
+
+        return builder;
+        // return MeshBuilder.FromCuboid(Vector3.zero, WorldChunk.Size);
+    }
+}
+
+public class MeshBuilder
+{
+    public List<Vector3> Vertices { get; private set; }
+    public List<Vector2> UVs { get; private set; }
+    public List<int> Triangles { get; private set; }
+
+    public MeshBuilder(int estimatedVertexCount = 0, int estimatedTriangleCount = 0)
+    {
+        Vertices = new(estimatedVertexCount);
+        UVs = new(estimatedVertexCount);
+        Triangles = new List<int>(capacity: estimatedTriangleCount * 3);
     }
 
-    private MeshBuilder GenerateCuboid(Vector3 size)
+    public MeshBuilder(Vector3[] vertices, Vector2[] uvs, int[] triangles)
     {
-        float w = size.x, h = size.y, d = size.z;
+        Vertices = new(vertices);
+        UVs = new(uvs);
+        Triangles = new(triangles);
+    }
+
+    public static MeshBuilder FromCuboid(Vector3 pos, Vector3 size)
+    {
+        float x1 = pos.x, y1 = pos.y, z1 = pos.z;
+        float x2 = x1 + size.x, y2 = y1 + size.y, z2 = z1 + size.z;
+
         Vector3[] vertices = {
-            new(0, 0, 0), new(w, 0, 0), new(w, h, 0), new(0, h, 0), // Front face
-            new(0, 0, d), new(w, 0, d), new(w, h, d), new(0, h, d)  // Back face
+            new(x1, y1, z1), new(x2, y1, z1), new(x2, y2, z1), new(x1, y2, z1), // Front face
+            new(x1, y1, z2), new(x2, y1, z2), new(x2, y2, z2), new(x1, y2, z2), // Back face
+            new(x1, y1, z1), new(x1, y2, z1), new(x1, y2, z2), new(x1, y1, z2), // Left face
+            new(x2, y1, z1), new(x2, y2, z1), new(x2, y2, z2), new(x2, y1, z2), // Right face
+            new(x1, y2, z1), new(x2, y2, z1), new(x2, y2, z2), new(x1, y2, z2), // Top face
+            new(x1, y1, z1), new(x2, y1, z1), new(x2, y1, z2), new(x1, y1, z2)  // Bottom face
         };
-
-        int[] triangles = {
-            0, 2, 1,   0, 3, 2,   // Front face
-            4, 5, 6,   4, 6, 7,   // Back face
-            0, 7, 3,   0, 4, 7,   // Left face
-            1, 2, 6,   1, 6, 5,   // Right face
-            2, 3, 7,   2, 7, 6,   // Top face
-            0, 1, 5,   0, 5, 4    // Bottom face
-        };
-
 
         Vector2[] uvs = {
             new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Front face
@@ -41,89 +76,75 @@ public class MeshGenerator2
             new(0, 0), new(1, 0), new(1, 1), new(0, 1)  // Bottom face
         };
 
-        return new MeshBuilder(vertices, triangles, uvs);
+        int[] triangles = {
+            00, 02, 01,   00, 03, 02, // Front face
+            04, 05, 06,   04, 06, 07, // Back face
+            08, 10, 09,   08, 11, 10, // Left face
+            12, 13, 14,   12, 14, 15, // Right face
+            16, 18, 17,   16, 19, 18, // Top face
+            20, 21, 22,   20, 22, 23  // Bottom face
+        };
+
+        return new MeshBuilder(vertices, uvs, triangles);
     }
 
-
-    /*
-    WorldChunk.ChunkSize;
-
-    int width = heightMap.GetLength(0);
-    int height = heightMap.GetLength(1);
-    float topLeftX = (width - 1) / -2f;
-    float topLeftZ = (height - 1) / 2f;
-
-    int meshIncrement = lod <= 0 ? 1 : lod * 2;
-    int verticesPerLine = (width - 1) / meshIncrement + 1;
-
-    MeshBuilder meshData = new MeshBuilder(verticesPerLine, verticesPerLine);
-    int vertexIndex = 0;
-
-    for (int y = 0; y < height; y += meshIncrement)
+    public void AddTriangle(int a, int b, int c)
     {
-        for (int x = 0; x < width; x += meshIncrement)
-        {
-            meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, newHeighCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
-            meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
-
-            if (x < width - 1 && y < height - 1)
-            {
-                meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
-                meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
-            }
-
-            vertexIndex++;
-        }
+        Triangles.Add(a);
+        Triangles.Add(b);
+        Triangles.Add(c);
     }
-
-    return meshData;
-    */
-}
-
-public class MeshBuilder
-{
-    public Vector3[] vertices;
-    public int[] triangles;
-    public Vector2[] uvs;
-
-    //int triangleIndex;
-
-    public MeshBuilder(Vector3[] vertices, int[] triangles, Vector2[] uvs)
-    {
-        this.vertices = vertices;
-        this.triangles = triangles;
-        this.uvs = uvs;
-    }
-
-
-
-    /*
-        public MeshBuilder(int meshWidth, int meshHeight)
-        {
-            vertices = new Vector3[meshWidth * meshHeight];
-            triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 2 * 3];
-            // uvs = new Vector2[meshWidth * meshHeight];
-        }*/
-
-    /*
-        public void AddTriangle(int a, int b, int c)
-        {
-            triangles[triangleIndex++] = a;
-            triangles[triangleIndex++] = b;
-            triangles[triangleIndex++] = c;
-        }
-        */
 
     public Mesh ToMesh()
     {
         Mesh mesh = new()
         {
-            vertices = vertices,
-            triangles = triangles,
-            uv = uvs,
+            vertices = Vertices.ToArray(),
+            uv = UVs.ToArray(),
+            triangles = Triangles.ToArray(),
         };
 
         mesh.RecalculateNormals();
         return mesh;
+    }
+}
+
+public class CellWallBuilder : MeshBuilder
+{
+    private readonly Vector3 _size;
+
+    private readonly Vector3Int _subDivs;
+
+    private readonly Vector3 _segm;
+
+    public CellWallBuilder(Vector3 size, Vector3Int subDivs)
+    {
+        _size = size;
+        _subDivs = subDivs;
+        _segm = new(size.x / subDivs.x, size.y / subDivs.y, size.z / subDivs.z);
+    }
+
+    /// <summary>
+    ///     +----X----+
+    ///    / 1  / 2  /|
+    ///   +----X----+ |   (-> dirX1toX2)
+    ///   |    |    | +  
+    ///   |    |    |/
+    ///   +----X----+
+    /// </summary>
+
+    public void AddWallX(int cellX1, int cellX2, int cellY, int cellZ, bool dirX1toX2)
+    {
+        float x2 = cellX2 * _segm.x;
+        float y1 = cellY * _segm.y;
+        float y2 = y1 + _segm.y;
+        float z1 = cellZ * _segm.z;
+        float z2 = z1 + _segm.z;
+
+        int v0 = Vertices.Count;
+        Vertices.AddRange(new Vector3[] { new(x2, y1, z1), new(x2, y2, z1), new(x2, y2, z2), new(x2, y1, z2) }); // Right face
+        UVs.AddRange(new Vector2[] { new(0, 0), new(1, 0), new(1, 1), new(0, 1) }); // Right face
+        if (dirX1toX2) Triangles.AddRange(new int[] { v0 + 0, v0 + 2, v0 + 1, v0 + 0, v0 + 3, v0 + 2 });
+        else Triangles.AddRange(new int[] { v0 + 0, v0 + 1, v0 + 2, v0 + 0, v0 + 2, v0 + 3 });
     }
 }
--- Assets/Clotzbergh/WorldChunk.cs
+++ Assets/Clotzbergh/WorldChunk.cs
@@ -1,13 +1,13 @@
+using System;
 using System.IO;
 using UnityEngine;
 
 public class WorldChunk
 {
-    public const int KlotzCountX = 32;
-
-    public const int KlotzCountY = 80;
-
-    public const int KlotzCountZ = 32;
+    public const int KlotzCountX = 32; // factors: 1, 2, 4,    8,     16,     32
+    public const int KlotzCountY = 80; // factors: 1, 2, 4, 5, 8, 10, 16, 20,     40, 80
+    public const int KlotzCountZ = 32; // factors: 1, 2, 4,    8,     16,     32
+    //                             common factors: 1, 2, 4,    8,     16
 
     public const int BorderSize = 1;
 
@@ -25,7 +25,7 @@ public class WorldChunk
         _dataRaw = new Klotz[KlotzCountRawX, KlotzCountRawY, KlotzCountRawZ];
     }
 
-    private void Fill(KlotzType t, int toHeight = KlotzCountY)
+    private void FloodFill(KlotzType t, int toHeight = KlotzCountY)
     {
         toHeight += BorderSize;
 
@@ -41,23 +41,67 @@ public class WorldChunk
         }
     }
 
-    public KlotzType GetRaw(int x, int y, int z) { return _dataRaw[x, y, z].Type; }
+    private void CoreFill(KlotzType t)
+    {
+        Vector3Int center = new(KlotzCountRawX / 2, KlotzCountRawY / 2, KlotzCountRawZ / 2);
 
-    public void SetRaw(int x, int y, int z, KlotzType t) { _dataRaw[x, y, z].Type = t; }
+        for (int z = 0; z < KlotzCountRawZ; z++)
+        {
+            for (int y = 0; y < KlotzCountRawY; y++)
+            {
+                for (int x = 0; x < KlotzCountRawX; x++)
+                {
+                    bool inCore =
+                         x > KlotzCountRawX / 4 && x < 3 * KlotzCountRawX / 4 &&
+                         y > KlotzCountRawY / 4 && y < 3 * KlotzCountRawY / 4 &&
+                         z > KlotzCountRawZ / 4 && z < 3 * KlotzCountRawZ / 4;
 
-    public KlotzType Get(int x, int y, int z) { return _dataRaw[x + BorderSize, y + BorderSize, z + BorderSize].Type; }
+                    if (inCore) _dataRaw[x, y, z].Type = t;
+                }
+            }
+        }
+    }
 
-    public void Set(int x, int y, int z, KlotzType t) { _dataRaw[x + BorderSize, y + BorderSize, z + BorderSize].Type = t; }
+    public Klotz GetRaw(int x, int y, int z) { return _dataRaw[x, y, z]; }
+
+    public void SetRaw(int x, int y, int z, Klotz t) { _dataRaw[x, y, z] = t; }
+
+    public Klotz Get(int x, int y, int z)
+    {
+        if (x < -BorderSize || x >= KlotzCountX + BorderSize ||
+            y < -BorderSize || y >= KlotzCountY + BorderSize ||
+            z < -BorderSize || z >= KlotzCountZ + BorderSize)
+            throw new ArgumentOutOfRangeException();
+
+        return _dataRaw[x + BorderSize, y + BorderSize, z + BorderSize];
+    }
+
+    public void Set(int x, int y, int z, Klotz t)
+    {
+        if (x < -BorderSize || x >= KlotzCountX + BorderSize ||
+            y < -BorderSize || y >= KlotzCountY + BorderSize ||
+            z < -BorderSize || z >= KlotzCountZ + BorderSize)
+            throw new ArgumentOutOfRangeException();
+
+        _dataRaw[x + BorderSize, y + BorderSize, z + BorderSize] = t;
+    }
 
     public static WorldChunk CreateEmpty()
     {
         return new WorldChunk();
     }
 
-    public static WorldChunk CreateFilled(KlotzType t, int toHeight = KlotzCountY)
+    public static WorldChunk CreateFloodFilled(KlotzType t, int toHeight = KlotzCountY)
     {
         WorldChunk chunk = new();
-        chunk.Fill(t, toHeight);
+        chunk.FloodFill(t, toHeight);
+        return chunk;
+    }
+
+    public static WorldChunk CreateCoreFilled(KlotzType t)
+    {
+        WorldChunk chunk = new();
+        chunk.CoreFill(t);
         return chunk;
     }
 
