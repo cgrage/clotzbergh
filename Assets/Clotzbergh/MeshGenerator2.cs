e2e29b8eb11eb07786b11c12f7d0fcc9dce803da
Christian Grage <christian.grage@philips.com>
2024-09-29 15:56:55 +0200

    Mesh generation on 3 axis

--- Assets/Clotzbergh/MeshGenerator2.cs
+++ Assets/Clotzbergh/MeshGenerator2.cs
@@ -21,9 +21,13 @@ public class MeshGenerator2
             {
                 for (int x = -1; x < WorldChunk.KlotzCountX; x++)
                 {
-                    bool b1t = worldChunk.Get(x + 0, y, z).IsSeeThrough;
-                    bool b2t = worldChunk.Get(x + 1, y, z).IsSeeThrough;
-                    if (b1t != b2t) { builder.AddWallX(x, x + 1, y, z, b1t); }
+                    bool bt = worldChunk.Get(x, y, z).IsSeeThrough;
+                    bool btx1 = worldChunk.Get(x + 1, y, z).IsSeeThrough;
+                    bool bty1 = worldChunk.Get(x, y + 1, z).IsSeeThrough;
+                    bool btz1 = worldChunk.Get(x, y, z + 1).IsSeeThrough;
+
+                    builder.AddWalls(new Vector3Int(x, y, z),
+                        bt != btx1, bt != bty1, bt != btz1, bt);
                 }
             }
         }
@@ -59,30 +63,30 @@ public class MeshBuilder
         float x2 = x1 + size.x, y2 = y1 + size.y, z2 = z1 + size.z;
 
         Vector3[] vertices = {
-            new(x1, y1, z1), new(x2, y1, z1), new(x2, y2, z1), new(x1, y2, z1), // Front face
-            new(x1, y1, z2), new(x2, y1, z2), new(x2, y2, z2), new(x1, y2, z2), // Back face
             new(x1, y1, z1), new(x1, y2, z1), new(x1, y2, z2), new(x1, y1, z2), // Left face
             new(x2, y1, z1), new(x2, y2, z1), new(x2, y2, z2), new(x2, y1, z2), // Right face
-            new(x1, y2, z1), new(x2, y2, z1), new(x2, y2, z2), new(x1, y2, z2), // Top face
-            new(x1, y1, z1), new(x2, y1, z1), new(x2, y1, z2), new(x1, y1, z2)  // Bottom face
+            new(x1, y1, z1), new(x1, y1, z2), new(x2, y1, z2), new(x2, y1, z1), // Bottom face
+            new(x1, y2, z1), new(x1, y2, z2), new(x2, y2, z2), new(x2, y2, z1), // Top face
+            new(x1, y1, z1), new(x2, y1, z1), new(x2, y2, z1), new(x1, y2, z1), // Back face
+            new(x1, y1, z2), new(x2, y1, z2), new(x2, y2, z2), new(x1, y2, z2), // Front face
         };
 
         Vector2[] uvs = {
-            new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Front face
-            new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Back face
             new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Left face
             new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Right face
+            new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Bottom face
             new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Top face
-            new(0, 0), new(1, 0), new(1, 1), new(0, 1)  // Bottom face
+            new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Back face
+            new(0, 0), new(1, 0), new(1, 1), new(0, 1), // Front face
         };
 
         int[] triangles = {
-            00, 02, 01,   00, 03, 02, // Front face
-            04, 05, 06,   04, 06, 07, // Back face
-            08, 10, 09,   08, 11, 10, // Left face
-            12, 13, 14,   12, 14, 15, // Right face
-            16, 18, 17,   16, 19, 18, // Top face
-            20, 21, 22,   20, 22, 23  // Bottom face
+            00+0, 00+2, 00+1,   00+0, 00+3, 00+2, // Left face
+            04+0, 04+1, 04+2,   04+0, 04+2, 04+3, // Right face
+            08+0, 08+2, 08+1,   08+0, 08+3, 08+2, // Bottom face
+            12+0, 12+1, 12+2,   12+0, 12+2, 12+3, // Top face
+            16+0, 16+2, 16+1,   16+0, 16+3, 16+2, // Back face
+            20+0, 20+1, 20+2,   20+0, 20+2, 20+3, // Front face
         };
 
         return new MeshBuilder(vertices, uvs, triangles);
@@ -125,26 +129,43 @@ public class CellWallBuilder : MeshBuilder
     }
 
     /// <summary>
-    ///     +----X----+
-    ///    / 1  / 2  /|
-    ///   +----X----+ |   (-> dirX1toX2)
-    ///   |    |    | +  
-    ///   |    |    |/
-    ///   +----X----+
+    /// Unity uses a left   +----+
+    /// handed coordinate  /    /|
+    /// system            +----+ |
+    ///     +----+----+   | Y2 | +     +----+
+    ///    /    /    /|   |    |/|    / Z1 /|
+    ///   +----+----+ |   +----+ |   +----+ |
+    ///   | X1 | X2 | +   | Y1 | +  /    /| +
+    ///   |    |    |/    |    |/  +----+ |/
+    ///   +----+----+     +----+   | Z2 | +
+    ///                            |    |/
+    ///                            +----+
     /// </summary>
-
-    public void AddWallX(int cellX1, int cellX2, int cellY, int cellZ, bool dirX1toX2)
+    public void AddWalls(Vector3Int cell, bool addWallXP1, bool addWallYP1, bool addWallZP1, bool wallDir)
     {
-        float x2 = cellX2 * _segm.x;
-        float y1 = cellY * _segm.y;
+        float x1 = cell.x * _segm.x;
+        float x2 = x1 + _segm.x;
+        float y1 = cell.y * _segm.y;
         float y2 = y1 + _segm.y;
-        float z1 = cellZ * _segm.z;
+        float z1 = cell.z * _segm.z;
         float z2 = z1 + _segm.z;
 
+        if (addWallXP1)
+            AddWall(new(x2, y1, z1), new(x2, y2, z1), new(x2, y2, z2), new(x2, y1, z2), wallDir); // Right face
+
+        if (addWallYP1)
+            AddWall(new(x1, y2, z1), new(x1, y2, z2), new(x2, y2, z2), new(x2, y2, z1), wallDir); // Top face
+
+        if (addWallZP1)
+            AddWall(new(x1, y1, z2), new(x2, y1, z2), new(x2, y2, z2), new(x1, y2, z2), wallDir); // Front face
+    }
+
+    private void AddWall(Vector3 corner1, Vector3 corner2, Vector3 corner3, Vector3 corner4, bool direction)
+    {
         int v0 = Vertices.Count;
-        Vertices.AddRange(new Vector3[] { new(x2, y1, z1), new(x2, y2, z1), new(x2, y2, z2), new(x2, y1, z2) }); // Right face
-        UVs.AddRange(new Vector2[] { new(0, 0), new(1, 0), new(1, 1), new(0, 1) }); // Right face
-        if (dirX1toX2) Triangles.AddRange(new int[] { v0 + 0, v0 + 2, v0 + 1, v0 + 0, v0 + 3, v0 + 2 });
+        Vertices.AddRange(new Vector3[] { corner1, corner2, corner3, corner4 });
+        UVs.AddRange(new Vector2[] { new(0, 0), new(1, 0), new(1, 1), new(0, 1) });
+        if (direction) Triangles.AddRange(new int[] { v0 + 0, v0 + 2, v0 + 1, v0 + 0, v0 + 3, v0 + 2 });
         else Triangles.AddRange(new int[] { v0 + 0, v0 + 1, v0 + 2, v0 + 0, v0 + 2, v0 + 3 });
     }
 }
