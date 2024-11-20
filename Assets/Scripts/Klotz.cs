5b1d575018002c9241043127a3c4108b8e02279d
Christian Grage <christian.grage@gmx.de>
2024-11-20 15:34:10 +0100

    Use bit-fields in WaveFunctionCollapseGenerator

--- Assets/Scripts/Klotz.cs
+++ Assets/Scripts/Klotz.cs
@@ -159,17 +159,6 @@ public static class KlotzKB
     {
         return true;
     }
-
-    public static readonly KlotzType[] AllGroundTypes = {
-        KlotzType.Plate1x1, KlotzType.Plate1x2, KlotzType.Plate1x3, KlotzType.Plate1x4, KlotzType.Plate1x6, KlotzType.Plate1x8,
-        KlotzType.Plate2x2, KlotzType.Plate2x3, KlotzType.Plate2x4, KlotzType.Plate2x6, KlotzType.Plate2x8,
-        KlotzType.Plate4x4, KlotzType.Plate4x6, KlotzType.Plate4x8,
-        KlotzType.Plate6x6, KlotzType.Plate6x8,
-        KlotzType.Plate8x8,
-        KlotzType.Brick1x1, KlotzType.Brick1x2, KlotzType.Brick1x3, KlotzType.Brick1x4, KlotzType.Brick1x6, KlotzType.Brick1x8,
-        KlotzType.Brick2x2, KlotzType.Brick2x3, KlotzType.Brick2x4, KlotzType.Brick2x6, KlotzType.Brick2x8,
-        KlotzType.Brick4x6,
-    };
 }
 
 /// <summary>
